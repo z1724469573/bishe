6522e7d7e58cd475cfc28d37c1cf044e8c94e798
bestwishes0203 <2109664977@qq.com>
2024-07-08 01:27:16 +0800

    update:v2 init

--- elder-front/src/router/index.ts
+++ elder-front/src/router/index.ts
@@ -1,19 +1,19 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: () => import('../views/HomeView.vue')
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: () => import('../views/pages/Home.vue')
+        },
+        {
+            path: '/about',
+            name: 'about',
+            component: () => import('../views/pages/About.vue')
+        }
+    ]
 })
 
 export default router
