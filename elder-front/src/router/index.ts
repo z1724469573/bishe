f0b5c38af3f64d98abd0c29bc82c935eb96537ed
bestwishes0203 <2109664977@qq.com>
2024-07-08 09:58:15 +0800

    update:router pages

--- elder-front/src/router/index.ts
+++ elder-front/src/router/index.ts
@@ -1,32 +1,53 @@
-import {createRouter, createWebHistory} from 'vue-router'
+import {createRouter, createWebHashHistory} from 'vue-router'
+
+export const routes = [
+    {
+        path: '/',
+        name: 'login',
+        component: () => import('../views/Login.vue')
+    },
+    {
+        path: '/layout',
+        name: 'layout',
+        redirect: '/home',
+        component: () => import('../views/Layout.vue'),
+        children: [
+            {
+                path: '/home',
+                name: '首页',
+                component: () => import('../views/pages/Home.vue')
+            },
+            {
+                path: '/active',
+                name: '公益活动',
+                component: () => import('../views/pages/Active.vue')
+            }, {
+                path: '/serve',
+                name: '养老服务',
+                component: () => import('../views/pages/Serve.vue')
+            },
+            {
+                path: '/elder',
+                name: '养老院',
+                component: () => import('../views/pages/Elder.vue')
+            },
+            {
+                path: '/myself',
+                name: '个人中心',
+                component: () => import('../views/pages/Myself.vue')
+            },
+            {
+                path: '/about',
+                name: '关于我们',
+                component: () => import('../views/pages/About.vue')
+            }
+        ]
+    },
+]
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
-        {
-            path: '/',
-            name: 'login',
-            component: () => import('../views/Login.vue')
-        },
-        {
-            path: '/layout',
-            name: 'layout',
-            redirect: '/home',
-            component: () => import('../views/Layout.vue'),
-            children: [
-                {
-                    path: '/home',
-                    name: 'home',
-                    component: () => import('../views/pages/Home.vue')
-                },
-                {
-                    path: '/about',
-                    name: 'about',
-                    component: () => import('../views/pages/About.vue')
-                }
-            ]
-        },
-    ]
+    history: createWebHashHistory(import.meta.env.BASE_URL),
+    routes
 })
 
 export default router
