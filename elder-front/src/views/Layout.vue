f910072fdcf1f6f4beead6fc7fd97bab35f93e3b
bestwishes0203 <2109664977@qq.com>
2024-07-08 09:40:44 +0800

    update:layout

--- elder-front/src/views/Layout.vue
+++ elder-front/src/views/Layout.vue
@@ -67,8 +67,8 @@
       <el-footer>
         <el-row>
           <el-col :span="24">
-            社区养老系统 客服电话 010-55779988
-            CopyRight
+            <p>社区养老系统 客服电话 010-55669988</p>
+            <p>CopyRight© 2001-2023 何福海版权所有</p>
           </el-col>
         </el-row>
       </el-footer>
