694aa44c15dbdd23ea2378899ba2617f4bfb1dd3
bestwishes0203 <2109664977@qq.com>
2024-07-10 17:04:03 +0800

    update:上传图片

--- elder-api/src/main/java/com/example/elderapi/common/controller/UploadController.java
+++ elder-api/src/main/java/com/example/elderapi/common/controller/UploadController.java
@@ -12,10 +12,10 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-@Tag(name="上传图片")
+@Tag(name = "上传图片")
 @RestController
 public class UploadController {
-    @Operation(summary = "上传图片到本地")
+    @Operation(summary = "上传图片")
     @PostMapping("/upload")
     public String upload(MultipartFile file) {
         if (file.isEmpty()) {
@@ -28,11 +28,13 @@ public class UploadController {
         String fileName = fileNamePrefix + fileNameSuffix;
         ApplicationHome applicationHome = new ApplicationHome(this.getClass());
         //String pre = applicationHome.getDir().getParentFile().getParentFile().getAbsolutePath() + "\\src\\main\\resources\\static\\images\\";
-        String pre = applicationHome.getDir().getParentFile().getParentFile().getParentFile().getAbsolutePath() + "\\spring-ui\\src\\assets\\avatar\\";
+        //String pre = applicationHome.getDir().getParentFile().getParentFile().getParentFile().getAbsolutePath() + "\\spring-ui\\src\\assets\\avatar\\";
+        String pre = "D:\\XFXH\\images\\";
         String path = pre + fileName;
         try {
             file.transferTo(new File(path));
-            String replace = path.replace(applicationHome.getDir().getParentFile().getParentFile().getParentFile().getAbsolutePath() + "\\spring-ui\\src", "/src");
+            //String replace = path.replace(applicationHome.getDir().getParentFile().getParentFile().getParentFile().getAbsolutePath() + "\\spring-ui\\src", "/src");
+            String replace = path.replace("D:\\XFXH\\images\\", "http://localhost/images/");
             return replace.replace("\\", "/");
         } catch (IOException e) {
             e.printStackTrace();
