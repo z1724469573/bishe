37043206aa1a8434b4507542ba5aa431a38a29e6
bestwishes0203 <2109664977@qq.com>
2024-07-11 21:08:34 +0800

    update:elder pages

--- elder-front/src/views/pages/Elder.vue
+++ elder-front/src/views/pages/Elder.vue
@@ -1,11 +1,47 @@
 <template>
-  <div></div>
+  <el-row justify="center">
+    <el-col :span="18">
+      <el-card style="width: calc(25% - 10px);float: left;margin: 5px;" v-for="item in elderList">
+        <img style="height: 180px;cursor: pointer;" :src="item.cover" alt=" " @click="handleElder(item)">
+        <p style="font-size: 16px;padding: 10px 20px;height: 20px;">{{ item.name }}</p>
+        <el-row align="middle" style="padding: 20px;">
+          <p style="font-size: 14px;height: 30px;color: red;">{{ item.price }}</p>
+        </el-row>
+      </el-card>
+    </el-col>
+  </el-row>
 </template>
 
 <script setup lang="ts">
+import {onMounted, ref} from "vue";
+import api from "@/api";
+import {useRouter} from 'vue-router'
 
+onMounted(() => {
+  getElderList();
+})
+
+const getElderList = () => {
+  api.elder.elderList().then((res) => {
+    elderList.value = res.data;
+    console.log(res.data);
+  })
+}
+
+const router = useRouter();
+const handleElder = (e) => {
+  const name = JSON.stringify(e.name);
+  router.push({
+    path: "/elderDetail",
+    query: {name},
+  })
+}
+
+const elderList = ref([]);
 </script>
 
 <style scoped>
-
+:deep(.el-card__body) {
+  padding: 0;
+}
 </style>
---
+++ elder-front/src/views/pages/ElderDetail.vue
@@ -0,0 +1,54 @@
+<template>
+  <el-row justify="center">
+    <el-col :span="18">
+      <el-row :gutter="20">
+        <el-col :span="12">
+          <img :src="elder.cover" alt=" ">
+        </el-col>
+        <el-col :span="12">
+          <h2>{{ elder.name }}</h2>
+          <el-descriptions title=" " :column="1">
+            <el-descriptions-item label=" ">
+              <el-tag size="small">{{ elder.tag }}</el-tag>
+            </el-descriptions-item>
+            <el-descriptions-item label=" ">
+              <b style="font-size: 22px;color: #c45656;">{{ elder.price }}</b>
+            </el-descriptions-item>
+            <el-descriptions-item label=" ">
+              <a style="font-size: 18px;">{{ elder.address }}</a>
+            </el-descriptions-item>
+            <el-descriptions-item label="联系电话：">
+              <b style="font-size: 18px;color: #0753a2;">{{ elder.phone }}</b>
+            </el-descriptions-item>
+            <el-descriptions-item label="成立时间：">{{ elder.date }}</el-descriptions-item>
+            <el-descriptions-item label="机构面积：">{{ elder.area }}</el-descriptions-item>
+            <el-descriptions-item label="房间数量：">{{ elder.bed }}</el-descriptions-item>
+          </el-descriptions>
+        </el-col>
+      </el-row>
+      <el-backtop :right="100" :bottom="100"/>
+      <v-md-editor mode="preview" v-model="elder.content"></v-md-editor>
+    </el-col>
+  </el-row>
+</template>
+
+<script setup lang="ts">
+import {onMounted, ref} from "vue";
+import {useRoute} from 'vue-router'
+import api from "@/api";
+
+const route = useRoute()
+const name = route.query.name;
+const elder = ref({});
+onMounted(() => {
+  api.elder.elderSearch(typeof name === "string" ? name.slice(1, name.length - 1) : "").then((res) => {
+    elder.value = res.data[0];
+  })
+})
+
+const elderList = ref([]);
+</script>
+
+<style scoped>
+
+</style>
